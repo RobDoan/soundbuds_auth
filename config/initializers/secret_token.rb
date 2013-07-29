e246f1dcac8613a6d11b07dfce2e4ddc7b3ad5a3
Quy Doan <quydoantran@gmail.com>
2013-07-29 08:13:15 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundbudSocial::Application.config.secret_token = 'a2cf1311ebd431c20b75dd6843415b6ccc25ce74cdec831fb8d562c9ef6beb58d3b19eae749d715c5eff956e537c56ea904fd4d20599ad6009a9f97f7f330356'
